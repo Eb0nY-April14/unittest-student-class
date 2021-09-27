8edcaabe06f2eef23bcc437a11e608c0c0564e4d
Eb0nY-April14 <laratemi14@gmail.com>
2021-09-27 14:35:29 +0000

    Add course_schedule functions for success & failed responses to the test_student.py file

--- student.py
+++ student.py
@@ -1,4 +1,5 @@
 from datetime import date, timedelta
+import requests
 
 class Student:
     """A Student class as base for method testing"""
@@ -9,8 +10,8 @@ class Student:
         # usage (i.e read-only) to other developers.
         self._first_name = first_name
         self._last_name = last_name
-        self._start_date = date.today()
-        self.end_date = date.today() + timedelta(days=365)
+        self._start_date = date.today()  # Should not be changed
+        self.end_date = date.today() + timedelta(days=365)   # Can be changed
         self.naughty_list = False  # initial value set to false
 
 
@@ -21,8 +22,7 @@ class Student:
     def full_name(self):
         return f"{self._first_name} {self._last_name}" 
 
-        # METHOD TO SET THE VALUE OF THE 'naughty_list' 
-        # PROPERTY TO True. 
+    # METHOD TO SET THE VALUE OF THE 'naughty_list' PROPERTY TO True. 
     def alert_santa(self):
         self.naughty_list = True
 
@@ -35,4 +35,24 @@ class Student:
     @property
     def email(self):
         return f"{self._first_name.lower()}.{self._last_name.lower()}@email.com" 
-   
+
+
+    def apply_extension(self, days):
+        self.end_date = self.end_date + timedelta(days=days)
+
+
+    # MOCKING HOW TO MAKE A CALL/REQUEST TO AN EXTERNAL API TO RETRIEVE A STUDENT'S COURSE SCHEDULE.
+    # THIS IS TO ALLOW DEVELOPERS TEST FOR SOMETHING BEYOND OUR CONTROL.
+    def course_schedule(self):
+        # We'll make a get request to our fictional API using a student’s_last_name &  _first_name as 
+        # part of the URL to get a student’s course schedule using 'requests.get' & store the response 
+        # in a variable called response.
+        response = requests.get(f"http://company.com/course-schedule/{self._last_name}/{self._first_name}")
+        # This will execute the 1st part of the 'if' statement if the request was successful.
+        # The 2nd part of the 'if' statement (i.e 'else' part) will be executed if the request/response failed.
+        if response.ok:
+            return response.text
+        else:
+            return "Something went wrong with the request!"
+        
+
--- test_student.py
+++ test_student.py
@@ -2,9 +2,26 @@ import unittest
 # This will import the class we created in 'student.py' file named 
 # 'Student' into our test file i.e test_student.py file.
 from student import Student
+from datetime import timedelta
+from unittest.mock import patch
 
 
 class TestStudent(unittest.TestCase):
+    # Adding the @classmethod decorator to a method & passing ‘cls’ as a 
+    # method parameter will make it a class method which acts on the class 
+    # instead of an instance of the class. This class method will be run 
+    # just once unlike the 'setUp' & 'tearDown' methods that runs at every 
+    # instance of the class.
+    @classmethod
+    def setUpClass(cls):
+        print('setUpClass')
+
+
+    @classmethod
+    def tearDownClass(cls):
+        print('tearDownClass')
+
+
     # Since most of our tests use the student instance which brings about 
     # code repition & violates the DRY principle. To prevent this, we'll 
     # create a 'setUp' function. Unittest gives us two methods that will 
@@ -52,6 +69,51 @@ class TestStudent(unittest.TestCase):
         print('test_alert_santa')
         self.student.alert_santa()
         self.assertTrue(self.student.naughty_list) 
+
+
+    def test_apply_extension(self):
+        # current value of end date is stored in old_end_date variable
+        old_end_date = self.student.end_date 
+        # We'll call the apply_extention method on student and pass in 
+        # an argument of 5 for the number of days required.
+        self.student.apply_extension(5)
+ 
+        self.assertEqual(self.student.end_date, old_end_date + timedelta(days=5)) 
+        # We can use assertNotEqual too as shown on the next line below:
+        # self.assertNotEqual(self.student.end_date , self.student.end_date + timedelta(days=5))
+
+
+    # We’ll use a context manager for our test method. we want to mock a get request in the student module,  
+    # so we can write with patch(‘student.requests.get’) as mocked_get: to set our context manager.   
+    # This creates an object called ‘mocked_get’ which we can use to test the 'get request' functionality.
+    def test_course_schedule_success(self):
+        # NOTE: Since we imported the student class at the top of this file, we have to use 
+        # 'student.requests.get' to access it.
+        with patch("student.requests.get") as mocked_get:
+            mocked_get.return_value.ok = True
+            mocked_get.return_value.text = "Success"
+
+            # Store the student’s course schedule in a variable called schedule. It holds 
+            # the returned response text for a successful call.
+            schedule = self.student.course_schedule()
+            # We'll use assertEqual to compare the variable “schedule” which holds the returned 
+            # response text for a successful call i.e the string “Success”.
+            self.assertEqual(schedule, "Success")
+
+
+    def test_course_schedule_failed(self):
+        # NOTE: Since we imported the student class at the top of this file, we have to use 
+        # 'student.requests.get' to access it.
+        with patch("student.requests.get") as mocked_get:
+            mocked_get.return_value.ok = False
+            # mocked_get.return_value.text = "Something went wrong with the request!"
+
+            # Store the student’s course schedule in a variable called schedule. It holds 
+            # the returned response text for a successful call.
+            schedule = self.student.course_schedule()
+            # We'll use assertEqual to compare the variable “schedule” which holds the returned 
+            # response text for a successful call i.e the string “Success”.
+            self.assertEqual(schedule, "Something went wrong with the request!")
 
 if __name__ == '__main__':
     unittest.main()    
